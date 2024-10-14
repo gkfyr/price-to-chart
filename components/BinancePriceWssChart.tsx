e0815372e07a8cb1b4c4268f105944477765d266
gkfyr <hrkairdrop@gmail.com>
2024-10-14 17:11:32 +0900

    test: fetch api in client side

--- components/BinancePriceWssChart.tsx
+++ components/BinancePriceWssChart.tsx
@@ -10,10 +10,16 @@ const EthCandleChartWss = () => {
   // REST API로 과거 캔들 데이터를 가져오는 함수
   const fetchCandleData = async () => {
     try {
-      const response = await fetch("/api/getCandleData");
+      const response = await fetch("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=150");
       const data = await response.json();
-      console.log(data);
-      return data.candles;
+      const formattedData = data.map((candle: any) => ({
+        time: candle[0] / 1000, // Unix timestamp (초 단위)
+        open: parseFloat(candle[1]),
+        high: parseFloat(candle[2]),
+        low: parseFloat(candle[3]),
+        close: parseFloat(candle[4]),
+      }));
+      return formattedData;
     } catch (error) {
       console.error("Error fetching candle data:", error);
       return [];
